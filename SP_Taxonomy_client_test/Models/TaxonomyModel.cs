e4c27019211487cddb11bb4678d517eef0d461bb
kientn-sesam <kien.nguyen@sesam.io>
2020-02-14 13:52:01 +0100

    Code optimization on fetching terms and creating term

---
+++ SP_Taxonomy_client_test/Models/TaxonomyModel.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace SP_Taxonomy_client_test.Models
+{
+    public class TaxonomyModel
+    {
+        public int Lcid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Termstore { get; set; }
+        public string TermGroup { get; set; }
+        public string Termset { get; set; }
+        public string Term { get; set; }
+        public IDictionary<string, string> LocalCustomProperty { get; set; }
+        public IDictionary<string, string> CustomProperty { get; set; }
+        public IDictionary<string, string> children { get; set; }
+        public List<Label> Labels { get; set; }
+    }
+    
+    public class Label 
+    {
+        public bool IsDefaultForLanguage { get; set; }
+        public int Language { get; set; }
+        public string Value { get; set; }
+    }
+}
